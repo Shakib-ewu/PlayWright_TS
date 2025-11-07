14f184dbe47da80dceb2e4f209d24864aa818810
Shakib-ewu <hossainsakib58@gmail.com>
2025-11-07 16:29:48 +0600

    New test case added

--- tests/getByPlaceholder.spec.ts
+++ tests/getByPlaceholder.spec.ts
@@ -4,6 +4,6 @@ test('Get By Placeholder Locator', async ({ page }) => {
   await page.goto('https://seabags.com/');
   const input = page.getByPlaceholder('Email');
   await expect(input).toBeVisible();
-  await input.fill('test@example.com');
+  await input.fill('seabags@example.com');
 });
          
