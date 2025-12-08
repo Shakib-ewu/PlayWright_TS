36eb9b715cab4a363adcc39692ba16a629987437
Shakib-ewu <hossainsakib58@gmail.com>
2025-12-08 14:12:03 +0600

    New test case added

---
+++ tests/hook.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('https://www.demoblaze.com/index.html');
+	await page.locator('#login2').click();
+	await page.locator('#loginusername').fill('pavanol');
+	await page.locator('#loginpassword').fill('test@123');
+	await page.locator("//button[normalize-space()='Log in']").click();
+	await page.waitForTimeout(1000);
+});
+
+test.afterEach(async ({ page }) => {
+	await page.locator('#logout2').click();
+});
+
+test('Hooks demo', async ({ page }) => {
+	const products = await page.locator('.hrefch').all();
+	await expect(products).toHaveLength(9);
+});
+
+test('Add to cart', async ({ page }) => {
+	// set up dialog handler BEFORE clicking
+	page.on('dialog', async dialog => {
+		expect(dialog.message()).toContain('Product added');
+		await dialog.accept();
+	});
+
+	await page.locator("//a[contains(text(),'Samsung galaxy s6')]").click();
+	await page.locator("//a[@class='btn btn-success btn-lg']").click();
+	await page.waitForTimeout(2000);
+});
